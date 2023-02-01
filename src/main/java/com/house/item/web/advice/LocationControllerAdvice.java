695c7bedcf6190b71665de5f9bc8342af54785d7
yrim <yrim113@gmail.com>
2023-02-02 00:24:56 +0900

    FEAT: NonExistentPlaceException ExceptionHandler 구현

--- src/main/java/com/house/item/web/advice/LocationControllerAdvice.java
+++ src/main/java/com/house/item/web/advice/LocationControllerAdvice.java
@@ -2,17 +2,16 @@ package com.house.item.web.advice;
 
 import com.house.item.common.ExceptionCodeMessage;
 import com.house.item.domain.ErrorResult;
+import com.house.item.exception.NonExistentPlaceException;
 import com.house.item.exception.NonExistentRoomException;
 import com.house.item.exception.NotLocationTypeRoomException;
-import com.house.item.web.AuthController;
-import com.house.item.web.UserController;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice(assignableTypes = {UserController.class, AuthController.class})
+@RestControllerAdvice
 @Slf4j
 public class LocationControllerAdvice {
 
@@ -21,6 +20,15 @@ public class LocationControllerAdvice {
     public ErrorResult nonExistentRoomException(NonExistentRoomException e) {
         return ErrorResult.builder()
                 .code(ExceptionCodeMessage.NON_EXISTENT_ROOM.code())
+                .message(e.getMessage())
+                .build();
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler
+    public ErrorResult nonExistentPlaceException(NonExistentPlaceException e) {
+        return ErrorResult.builder()
+                .code(ExceptionCodeMessage.NON_EXISTENT_PLACE.code())
                 .message(e.getMessage())
                 .build();
     }
