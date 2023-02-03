c3118b2e69f448e93b9599babb60a0d2a61d0ab1
yrim <yrim113@gmail.com>
2023-02-03 18:48:03 +0900

    FEAT: Label 엔티티에 ItemLabel list orphanRemoval = true 으로 생성

--- src/main/java/com/house/item/entity/Label.java
+++ src/main/java/com/house/item/entity/Label.java
@@ -1,14 +1,17 @@
 package com.house.item.entity;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Builder
 public class Label {
 
     @Id
@@ -20,4 +23,17 @@ public class Label {
     private User user;
 
     private String name;
+
+    @OneToMany(mappedBy = "label", orphanRemoval = true)
+    private List<ItemLabel> itemLabels = new ArrayList<>();
+
+    @Builder
+    private Label(Long labelNo, User user, String name, List<ItemLabel> itemLabels) {
+        this.labelNo = labelNo;
+        this.user = user;
+        this.name = name;
+        if (itemLabels != null) {
+            this.itemLabels = itemLabels;
+        }
+    }
 }
