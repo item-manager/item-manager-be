a83e4ca05fa34f6b56f445c28160098fb2e1d3d9
yrim <yrim113@gmail.com>
2023-02-13 16:35:26 +0900

    TEST: 물품_구매(), 물품_사용(), 사용_수량이_물품_수량을_초과() 추가

---
+++ src/test/java/com/house/item/service/QuantityLogServiceTest.java
@@ -0,0 +1,147 @@
+package com.house.item.service;
+
+import com.house.item.domain.AddItemQuantityRQ;
+import com.house.item.domain.SessionUser;
+import com.house.item.domain.SubtractItemQuantityRQ;
+import com.house.item.entity.*;
+import com.house.item.exception.SubtractCountExceedItemQuantityException;
+import com.house.item.util.SessionUtils;
+import com.house.item.web.SessionConst;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.time.LocalDateTime;
+
+@SpringBootTest
+@Transactional
+class QuantityLogServiceTest {
+    @Autowired
+    QuantityLogService quantityLogService;
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void 물품_구매() throws Exception {
+        //given
+        User user = createSessionUser();
+        Item item = createItem(user, 0);
+        Long itemNo = item.getItemNo();
+
+        AddItemQuantityRQ addItemQuantityRQ = AddItemQuantityRQ.builder()
+                .mall("mall")
+                .date(LocalDateTime.now())
+                .unitPrice(1000)
+                .count(10)
+                .build();
+
+        //when
+        int resultQuantity = quantityLogService.addItemQuantity(itemNo, addItemQuantityRQ);
+
+        //then
+        Item findItem = em.find(Item.class, itemNo);
+        Assertions.assertThat(findItem.getQuantity()).isEqualTo(resultQuantity);
+    }
+
+    @Test
+    void 물품_사용() throws Exception {
+        //given
+        User user = createSessionUser();
+        Item item = createItem(user, 10);
+        Long itemNo = item.getItemNo();
+
+        SubtractItemQuantityRQ subtractItemQuantityRQ = SubtractItemQuantityRQ.builder()
+                .date(LocalDateTime.now())
+                .count(10)
+                .build();
+
+        //when
+        int resultQuantity = quantityLogService.subtractItemQuantity(itemNo, subtractItemQuantityRQ);
+
+        //then
+        Item findItem = em.find(Item.class, itemNo);
+        Assertions.assertThat(findItem.getQuantity()).isEqualTo(resultQuantity);
+    }
+
+    @Test
+    void 사용_수량이_물품_수량을_초과() throws Exception {
+        //given
+        User user = createSessionUser();
+        Item item = createItem(user, 1);
+        Long itemNo = item.getItemNo();
+
+        SubtractItemQuantityRQ subtractItemQuantityRQ = SubtractItemQuantityRQ.builder()
+                .date(LocalDateTime.now())
+                .count(2)
+                .build();
+
+        //when
+        Assertions.assertThatThrownBy(() -> quantityLogService.subtractItemQuantity(itemNo, subtractItemQuantityRQ))
+                .isInstanceOf(SubtractCountExceedItemQuantityException.class);
+
+        //then
+    }
+
+    User createSessionUser() {
+        User user = User.builder()
+                .id("id")
+                .password("pw")
+                .salt("salt")
+                .username("name")
+                .build();
+        em.persist(user);
+
+        SessionUser sessionUser = SessionUser.builder()
+                .userNo(user.getUserNo())
+                .username(user.getUsername())
+                .build();
+
+        SessionUtils.setAttribute(SessionConst.LOGIN_USER, sessionUser);
+        return user;
+    }
+
+    Location createLocation(User user) {
+        Location room = Location.builder()
+                .user(user)
+                .name("room")
+                .build();
+        em.persist(room);
+
+        Location place = Location.builder()
+                .user(user)
+                .name("place")
+                .room(room)
+                .build();
+        em.persist(place);
+
+        return place;
+    }
+
+    Item createItem(User user, int quantity) {
+        Item item = Item.builder()
+                .user(user)
+                .name("item")
+                .type(ItemType.CONSUMABLE)
+                .location(createLocation(user))
+                .quantity(quantity)
+                .build();
+        em.persist(item);
+
+        return item;
+    }
+
+    ItemQuantityLog getItemQuantityLog(Item item, QuantityType type, String mall, LocalDateTime date, int price, int count) {
+        return ItemQuantityLog.builder()
+                .item(item)
+                .type(type)
+                .mall(mall)
+                .date(date)
+                .price(price)
+                .count(count)
+                .build();
+    }
+
+}
