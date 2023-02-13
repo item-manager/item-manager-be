eedb710f5f67a2cbd3adaed306367877a9926170
yrim <yrim113@gmail.com>
2023-02-13 17:20:26 +0900

    ADD: count 최소값 0 검증 추가

--- src/main/java/com/house/item/domain/ConsumeItemRQ.java
+++ src/main/java/com/house/item/domain/ConsumeItemRQ.java
@@ -3,11 +3,13 @@ package com.house.item.domain;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.validation.constraints.Min;
 import java.time.LocalDateTime;
 
 @Getter
 @Builder
 public class ConsumeItemRQ {
     private LocalDateTime date;
+    @Min(0)
     private int count;
 }
