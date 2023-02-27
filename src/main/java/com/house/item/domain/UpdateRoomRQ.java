0cfb9d626d9a299df0ad9b26305100351007791a
yrim <yrim113@gmail.com>
2023-02-27 14:39:12 +0900

    FIX: RequestBody는 NoArgsConstructor로 @Builder 제거

--- src/main/java/com/house/item/domain/UpdateRoomRQ.java
+++ src/main/java/com/house/item/domain/UpdateRoomRQ.java
@@ -1,12 +1,10 @@
 package com.house.item.domain;
 
-import lombok.Builder;
 import lombok.Getter;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter
-@Builder
 public class UpdateRoomRQ {
     @NotBlank
     private String name;
