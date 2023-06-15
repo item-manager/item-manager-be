6f7a5108ebcef6415c6aeba60a0826c02b2aa507
yurim <yrim113@gmail.com>
2023-06-15 18:07:39 +0900

    Refactor: 개발용 데이터 입력 로직 제거

--- src/main/java/com/house/item/ItemApplication.java
+++ src/main/java/com/house/item/ItemApplication.java
@@ -1,14 +1,8 @@
 package com.house.item;
 
-import com.house.item.dev.TestDataInit;
-import com.house.item.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-
-import javax.persistence.EntityManager;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -17,11 +11,4 @@ public class ItemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ItemApplication.class, args);
 	}
-
-	@Bean
-	@Profile("local")
-	public TestDataInit testDataInit(EntityManager em, UserService userService) {
-		return new TestDataInit(em, userService);
-	}
-
 }
