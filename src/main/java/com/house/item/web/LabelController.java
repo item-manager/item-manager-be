af5e4e65bc5617ab2424a173a32c20d130615323
yurim <yrim113@gmail.com>
2023-06-26 16:58:10 +0900

    Refactor: #91 - Label Session에서 User 조회 controller로 이동

--- src/main/java/com/house/item/web/LabelController.java
+++ src/main/java/com/house/item/web/LabelController.java
@@ -1,10 +1,32 @@
 package com.house.item.web;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.house.item.common.ExceptionCodeMessage;
-import com.house.item.domain.*;
+import com.house.item.domain.CreateLabel;
+import com.house.item.domain.CreateLabelRQ;
+import com.house.item.domain.CreateLabelRS;
+import com.house.item.domain.ErrorResult;
+import com.house.item.domain.LabelRS;
+import com.house.item.domain.Result;
+import com.house.item.domain.SessionUser;
+import com.house.item.domain.UpdateLabelRQ;
 import com.house.item.entity.Label;
+import com.house.item.entity.User;
 import com.house.item.exception.NonUniqueLabelNameException;
 import com.house.item.service.LabelService;
+import com.house.item.service.UserService;
+import com.house.item.util.SessionUtils;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
@@ -12,9 +34,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @Slf4j
@@ -22,6 +41,7 @@ import java.util.List;
 @RequestMapping("/labels")
 public class LabelController {
     private final LabelService labelService;
+    private final UserService userService;
 
     @ApiResponse(
             responseCode = "400",
@@ -35,14 +55,17 @@ public class LabelController {
     @Operation(summary = "라벨 생성")
     @PostMapping
     public Result<CreateLabelRS> createLabel(@RequestBody CreateLabelRQ createLabelRQ) throws NonUniqueLabelNameException {
+        SessionUser sessionUser = SessionUtils.getSessionUser();
+        User user = userService.getUser(sessionUser.getUserNo());
+
         CreateLabel createLabel = CreateLabel.builder()
-                .name(createLabelRQ.getName())
-                .build();
-        Long labelNo = labelService.createLabel(createLabel);
+            .name(createLabelRQ.getName())
+            .build();
+        Long labelNo = labelService.createLabel(createLabel, user);
 
         CreateLabelRS createLabelRS = CreateLabelRS.builder()
-                .labelNo(labelNo)
-                .build();
+            .labelNo(labelNo)
+            .build();
         return Result.<CreateLabelRS>builder()
                 .data(createLabelRS)
                 .build();
@@ -51,12 +74,15 @@ public class LabelController {
     @Operation(summary = "라벨 목록 조회")
     @GetMapping
     public Result<List<LabelRS>> getLabels() {
-        List<Label> labels = labelService.getLabels();
+        SessionUser sessionUser = SessionUtils.getSessionUser();
+        User user = userService.getUser(sessionUser.getUserNo());
+
+        List<Label> labels = labelService.getLabels(user);
 
         List<LabelRS> labelRSList = labelService.labelToLabelRS(labels);
         return Result.<List<LabelRS>>builder()
-                .data(labelRSList)
-                .build();
+            .data(labelRSList)
+            .build();
     }
 
     @ApiResponse(
@@ -72,12 +98,15 @@ public class LabelController {
     @Operation(summary = "라벨 수정")
     @PatchMapping("/{labelNo}")
     public Result<Void> patchLabel(@PathVariable Long labelNo, @RequestBody UpdateLabelRQ updateLabelRQ) {
-        labelService.updateLabel(labelNo, updateLabelRQ);
+        SessionUser sessionUser = SessionUtils.getSessionUser();
+        User user = userService.getUser(sessionUser.getUserNo());
+
+        labelService.updateLabel(labelNo, updateLabelRQ, user);
 
         return Result.<Void>builder()
-                .code(200)
-                .message("ok")
-                .build();
+            .code(200)
+            .message("ok")
+            .build();
     }
 
     @ApiResponse(
@@ -92,11 +121,14 @@ public class LabelController {
     @Operation(summary = "라벨 제거")
     @DeleteMapping("/{labelNo}")
     public Result<Void> deleteLabel(@PathVariable Long labelNo) {
-        labelService.deleteLabel(labelNo);
+        SessionUser sessionUser = SessionUtils.getSessionUser();
+        User user = userService.getUser(sessionUser.getUserNo());
+
+        labelService.deleteLabel(labelNo, user);
 
         return Result.<Void>builder()
-                .code(200)
-                .message("ok")
-                .build();
+            .code(200)
+            .message("ok")
+            .build();
     }
 }
